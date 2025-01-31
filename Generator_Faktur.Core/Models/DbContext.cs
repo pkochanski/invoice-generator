af98cbb87db3d203fc1cdcc38975777c48666446
Piotr <piotr.kochanski0@gmail.com>
2025-01-31 15:50:24 +0000

    Update DbContext.cs

--- Generator_Faktur.Core/Models/DbContext.cs
+++ Generator_Faktur.Core/Models/DbContext.cs
@@ -9,16 +9,16 @@ namespace Generator_Faktur.Core.Models
     {
         public static ObservableCollection<Client> Clients = new ObservableCollection<Client> { 
             new Client {
-                Name="Billennium S.A.",
-                Adress="00-667 Warszawa, ul. Koszykowa 61",
-                NIP="5252259585",
+                Name="xxxxxxxx",
+                Adress="xxxxxxxxxx",
+                NIP="xxxxxxxxxxxxxxx",
                 Id = 1
             },
             new Client
             {
-                Name="Oodit",
-                Adress="Anderlechtlaan 181, 1066HM Amsterdam",
-                VatEU="NL859127916B01",
+                Name="xxxx",
+                Adress="xxxxxxxxxxxxxx",
+                VatEU="x",
                 Id = 2
             }
         };
@@ -26,11 +26,11 @@ namespace Generator_Faktur.Core.Models
         public static Issuer Issuer = new Issuer
         {
             Name="Piotr Kochański",
-            Adress= "87-312 Pokrzydowo,Pokrzydowo 226",
-            BankAccountNumber = "82 1050 1807 1000 0097 2287 4915",
-            NIP = "7393889757",
-            VatEU = "PL7393889757",
-            EuroBankAccountNumber = "PL 22 1050 1807 1000 0097 6499 3201"
+            Adress= "xxxxxxxxxxxxxxx",
+            BankAccountNumber = "0000000000000000000000000",
+            NIP = "xxxxxxxxxxxx",
+            VatEU = "xxxxxxxxxx",
+            EuroBankAccountNumber = "PL 0000000000000000000000"
         };
 
         public static ExchangeRateSeries EuroExchangeRate;
